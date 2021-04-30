1bbb02bb1f7e3c098aae961b961be432d7c735f1
Okpix <74378275+Okpix@users.noreply.github.com>
2021-04-30 11:42:10 +0200

    Initialisation du projet avec la structure html de base

---
+++ script.js
@@ -0,0 +1,18 @@
+var button = document.getElementById('game')
+let scoreG = document.getElementById('scoreG1');
+
+function evenement() {
+  alert('Nouvelle partie !')
+}
+
+button.addEventListener('click', evenement)
+
+
+
+
+
+
+
+
+
+
