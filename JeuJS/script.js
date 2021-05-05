8a309ee7cb5d78b3a66dfe67e918d52333759cd3
Okpix <74378275+Okpix@users.noreply.github.com>
2021-05-05 11:56:34 +0200

    ajout de la fonction lancer le dé + début test de la fonction hold mais bug sur l'adition (string/int)

--- JeuJS/script.js
+++ JeuJS/script.js
@@ -1,47 +1,70 @@
-var button = document.getElementById('game')
+/*var button = document.getElementById('game');
 
 function evenement() {
   alert('Vous avez décidé de lancer une nouvelle partie !')
-}
+};
 
-button.addEventListener('click', evenement)
+button.addEventListener('click', evenement);
 
 
 
 
 /* début de partie */
 
-const scoreP1 = document.getElementById('scoreP1');
-const scoreP2 = document.getElementById('scoreP2');
-const current1 = document.getElementById('current1');
-const current2 = document.getElementById('current2');
+var scoreP1 = document.getElementById('scoreP1');
+var scoreP2 = document.getElementById('scoreP2');
+var current1 = document.getElementById('current1');
+var current2 = document.getElementById('current2');
 
 
 
 function start() {
-  const begincount = 0;
+  var begincount = 0;
   
-
   scoreP1.innerHTML = begincount;
   scoreP2.innerHTML = begincount;
   current1.innerHTML = begincount;
   current2.innerHTML = begincount;
+  dede.innerHTML = begincount;
 
 };
 
 
 /* lancer le dé */
-const dede = document.getElementById('dede');
-var button = document.getElementById('roll');
 
-function getRandomInt() {
-  return Math.floor(Math.random() * 6) +1;
+/*var dede = document.getElementById('dede');
+var testbtn = document.getElementById('roll');
+
+
+
+function calcul(){
+  return Math.floor(Math.random() * 6) + 1;
+};
+
+dede.innerHTML = calcul();
+
+console.log(dede);*/
+
+var tabNbr = [];
+for (i = 1; i <= 6; i++){
+	tabNbr.push(i)
 }
 
-console.log(getRandomInt());
+var nbrAleatoire = function(nbr){
+	return nbr[Math.floor(Math.random() * nbr.length)];
+}
 
-dede.innerHTML = getRandomInt();
+var roll = function(){
+	var a = nbrAleatoire(tabNbr);
+	dede.innerHTML = (a);
+  console.log(dede.innerHTML)
+}
 
 
-/* expected output: 1 2 3 4 5 6 */
+/* garder un dé */ 
 
+function hold(){
+
+  var hold = current1.innerHTML + dede.innerHTML;
+  current1.innerHTML = hold;
+}
